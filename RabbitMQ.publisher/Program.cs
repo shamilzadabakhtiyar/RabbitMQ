49616b4856ab47054d76cc665768f73527d74900
Admin <Admin@DESKTOP-34CAHMM>
2024-06-07 10:44:36 +0400

    Add project files.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,18 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://wzrfvrnm:Ty8g7emkfwJ0BqZdd2HoZ92oSbJ23FeH@beaver.rmq.cloudamqp.com/wzrfvrnm");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare("hello-queue", true, false, false);
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"message {x}";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    Console.WriteLine($"Message sent: {message}");
+});
+
+Console.ReadLine();
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://wzrfvrnm:Ty8g7emkfwJ0BqZdd2HoZ92oSbJ23FeH@beaver.rmq.cloudamqp.com/wzrfvrnm");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+//channel.QueueDeclare("hello-queue", true, false, false);
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("hello-queue", false, consumer);
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("Incoming message: " + message);
+    channel.BasicAck(e.DeliveryTag, false);
+    Thread.Sleep(1000);
+};
+
+Console.ReadLine();
